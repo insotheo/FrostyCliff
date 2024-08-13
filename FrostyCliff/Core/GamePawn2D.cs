c984a9c9bab06edcc9f2d224e271b9ebf174afbe
insotheo <maxim.insotheo@gmail.com>
2024-08-13 19:44:09 +0400

    Removing physics 3

--- FrostyCliff/Core/GamePawn2D.cs
+++ FrostyCliff/Core/GamePawn2D.cs
@@ -1,5 +1,4 @@
 ﻿using FrostyCliff.Graphics;
-using FrostyCliff.Physics;
 using System;
 
 namespace FrostyCliff.Core
@@ -8,7 +7,6 @@ namespace FrostyCliff.Core
     {
         public Transform2D Transform;
         public RendererObject2D RendererObject;
-        public PhysicsObject2D PhysicsObject { get; internal set; }
 
         public GamePawn2D(Transform2D transform)
         {
@@ -19,11 +17,6 @@ namespace FrostyCliff.Core
         internal void OnPawnUpdate(double dt) => OnUpdate(dt);
 
         public void Dispose() { }
-
-        public void CreatePhysicsObject(PhysicsBodyType bodyType, float mass)
-        {
-            PhysicsObject = new PhysicsObject2D(Transform, bodyType, mass);
-        }
 
         protected virtual void OnBegin() { }
         protected virtual void OnUpdate(double deltaTime) { }
